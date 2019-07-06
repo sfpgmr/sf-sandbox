36d960ac61e35c2f6548fe7dcb41602b55486e75
sfpgmr <sfpgmr@dummy.com>
2019-07-06 21:00:47 +0900

    update

---
+++ gamepad/current/build/index.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  //The MIT License (MIT)
+
+  // メイン
+  window.addEventListener('load', async ()=>{
+
+    var WIDTH = window.innerWidth , HEIGHT = window.innerHeight;
+
+    window.addEventListener( 'resize', ()=>{
+          WIDTH = window.innerWidth;
+          HEIGHT = window.innerHeight;
+    }
+    , false );
+    
+  });
+
+}());
