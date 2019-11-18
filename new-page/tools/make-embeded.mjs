ce8af9a297ebea751f5a64ca3db3f88bd4e3e622
sfpmr <sfpg@sfpg.com>
2019-11-19 06:31:09 +0900

    update

--- new-page/tools/make-embeded.mjs
+++ new-page/tools/make-embeded.mjs
@@ -2,6 +2,7 @@ import request from 'request-promise-native';
 import fs from 'fs';
 import YouTube from 'youtube-node';
 import util from 'util';
+import puppeteer from 'puppeteer';
 
 
 try {
@@ -15,14 +16,36 @@ const youtube = {
 };
 (async()=>{
   const tweets = JSON.parse(await fs.promises.readFile('./data/tweets.json'));
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
   for(const tweet of tweets){
     if(tweet.entities.urls){
       const urls = tweet.entities.urls;
       for(const url of urls){
         const m = (/youtu\.?be\/([^\/]+)$/i).exec(url.display_url);
         if(m){
-          const id = m[1];
-          url.youtube = await youtube.getById(id);
+          try {
+            const id = m[1];
+            url.youtube = await youtube.getById(id);
+          } catch (e) {
+            console.log(e);
+          }
+        } else {
+          console.log(url.url);
+          try {
+
+            const p = await page.goto(url.expanded_url);
+            const tag = await page.evaluate(()=>{
+              return  {
+                title:document.title,
+                description:document.querySelectorAll('meta[name =  "description"]')[0].content
+              }
+            }); 
+            console.log(tag);
+            //url.ogp = ogp;
+          } catch (e) {
+            console.log(e);
+          }
         }
       }
     }
@@ -44,6 +67,6 @@ const youtube = {
   await fs.promises.writeFile('./data/tweet2.json',JSON.stringify(tweets,null,1),'utf8');
 })();
 } catch (e) {
-  console.log(e);
+  console.log('Error:',e);
 }
 
