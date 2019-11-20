f6e7a87536bc86f5c040150303c049e0d06f16f6
sfpmr <sfpg@sfpg.com>
2019-11-21 06:24:30 +0900

    update

--- new-page/tools/make-embeded.mjs
+++ new-page/tools/make-embeded.mjs
@@ -6,6 +6,9 @@ import puppeteer from 'puppeteer';
 import jsdom from 'jsdom';
 const {JSDOM} = jsdom;
 
+process.setMaxListeners(Infinity); // <== Important line
+
+
 
 try {
 const yt = new YouTube();
@@ -19,7 +22,6 @@ const youtube = {
 (async()=>{
   const tweets = JSON.parse(await fs.promises.readFile('./data/tweets.json'));
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
   for(const tweet of tweets){
     if(tweet.entities.urls){
       const urls = tweet.entities.urls;
@@ -33,14 +35,33 @@ const youtube = {
             console.log(e);
           }
         } else {
-          console.log(url.url);
+
           try {
 
+            const page = await browser.newPage();
             const p = await page.goto(url.expanded_url);
+            console.info(url.expanded_url);
+
+            page.on('console', msg => {
+              if(msg.type() == 'log'){
+                console.log(msg.text());
+              }
+            });
+
             const meta = await page.evaluate(()=>{
-              let description = document.querySelector('meta[name =  "description"]');
-              description = description ? description.content : '';
+              function isObject(o){
+                return (o instanceof Object && !(o instanceof Array)) ? true : false;
+              }
+              // meta description
+              let description = document.querySelector('meta[name = "description"]');
+              description = description ? description.content : null;
+              // meta keywords
+              let keywords = document.querySelector('meta[name="keywords"]');
+              keywords = keywords ? keywords.content : null;
+
+
               let twitterMeta = document.querySelectorAll('meta[name ^= "twitter:"]'),twitter;
+
               // twitter-card
               if(twitterMeta) {
                 twitter = {};
@@ -48,12 +69,20 @@ const youtube = {
                     let tm_ns = tm.name.split(':').slice(1);
                     if(tm_ns.length > 1){
                       tm_ns.reduce((p,v,i,a)=>{
-                        if(!(v in p)){
-                          if(i == (a.length - 1)){
-                            p[v] = tm.content;
-                          } else {
-                            p[v] = {};
+                        console.log(`@prop@:${tm.name} ${p} ${v}`);
+                        if(isObject(p)){
+                          if(!(v in p)){
+                            if(i == (a.length - 1)){
+                              p[v] = tm.content;
+                            } else {
+                              p[v] = {};
+                            }
                           }
+                        } else {
+                          const pp = p;
+                          p = {};
+                          p[p] = pp;
+                          p[v] = {};
                         }
                         return p[v];
                         },twitter);
@@ -66,17 +95,26 @@ const youtube = {
               let ogps = document.querySelectorAll('meta[name ^= "og:"]');
               let og;
               if(ogps){
-                og = {};
+                try {
+                  og = {};
                 for(const o of ogps){
                   let og_ns = o.name.split(':').slice(1);
                   if(og_ns.length > 1){
                     og_ns.reduce((p,v,i,a)=>{
-                      if(!(v in p)){
-                        if(i == (a.length - 1)){
-                          p[v] = o.content;
-                        } else {
-                          p[v] = {};
+                      console.log(`@prop@:${o.name} ${p} ${v}`);
+                      if(isObject(p)){
+                        if(!(v in p)){
+                          if(i == (a.length - 1)){
+                            p[v] = tm.content;
+                          } else {
+                            p[v] = {};
+                          }
                         }
+                      } else {
+                        const pp = p;
+                        p = {};
+                        p[p] = pp;
+                        p[v] = {};
                       }
                       return p[v];
                     },og);
@@ -84,14 +122,23 @@ const youtube = {
                     og[og_ns[0]] = o.content;
                   }
                 }
+              } catch(e) {
+                console.log(e);
+              }
+  
               }
               // json-ld
               let json_ld_tags = document.querySelectorAll('script[type="application/ld+json"]'),json_lds;
               if(json_ld_tags){
                 json_lds = [];
                 for(const j of json_ld_tags){
-                  const json =  JSON.parse(j.textContent);
-                  json_lds.push(json);
+                  try {
+                    console.log(j.textContent);
+                    const json =  JSON.parse(j.textContent);
+                    json_lds.push(json);
+                  } catch (e) {
+                    console.log(e);                    
+                  }
                 }
               }
 
@@ -99,11 +146,14 @@ const youtube = {
               return  {
                 title:document.title,
                 description:description,
+                keywords:keywords,
                 twitter:twitter,
                 og:og,
                 json_lds:json_lds
               }
             });
+
+            
             // const {window} = await JSDOM.fromURL(url.expanded_url);
             // const {document} = window;
             // let description = document.querySelector('meta[name =  "description"]').content;
@@ -112,6 +162,7 @@ const youtube = {
             // console.log(title,description);
             //url.ogp = ogp;
             url.meta = meta;
+            await page.close();
             //console.log(meta);
           } catch (e) {
             console.log(e);
@@ -135,6 +186,7 @@ const youtube = {
     // 
   }
   await fs.promises.writeFile('./data/tweet2.json',JSON.stringify(tweets,null,1),'utf8');
+  //await page.close();
   await browser.close();
 })();
 } catch (e) {
