11c12b14652de2a25333b0e739866fdaeb631d30
sfpmr <sfpg@sfpg.com>
2019-11-24 10:34:57 +0900

    update

--- new-page/tools/twitter.mjs
+++ new-page/tools/twitter.mjs
@@ -18,13 +18,43 @@ try {
   // });
 
   
-  const params = { screen_name: 'sfpgmr',trim_user:false,include_rts:true,count:200,        tweet_mode:'extended'
-};
+  const params = { 
+    screen_name: 'sfpgmr',
+    trim_user:false,
+    include_rts:true,
+    count:200,
+    tweet_mode:'extended'
+  };
+
   const getTweets = util.promisify(client.get.bind(client));
-  
+  const tweetData = [];
   (async ()=>{
-    const tweets = await getTweets('statuses/user_timeline', params);
-    await fs.promises.writeFile('./data/tweets.json', JSON.stringify(tweets, null, 2), 'utf8');
+    while(true){
+      const tweets = await getTweets('statuses/user_timeline', params);
+      if(tweets.length == 0) {
+        break;
+      }
+      if(params.max_id){
+        tweetData.push(...(tweets.slice(1)));
+      } else {
+        tweetData.push(...tweets);
+      }
+      // tweets.sort((a,b)=>{
+      //   if(a.id < b.id) {
+      //     return 1;
+      //   }
+
+      //   if(a.id > b.id) {
+      //     return -1;
+      //   }        
+
+      //   return 0;
+      // });
+      const last = tweets[tweets.length - 1];
+      params.max_id = last.id;
+      console.log(tweets.length,last.id,last.created_at);
+    }
+    await fs.promises.writeFile('./data/tweets.json', JSON.stringify(tweetData, null, 2), 'utf8');
   })();
 } catch (error){
   console.error(error);
