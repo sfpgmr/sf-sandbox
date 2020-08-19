746e46555b34d6fab18a001e5bbcbaedcdadd2c1
sfpgmr <sfpgmr@example.com>
2020-08-20 07:52:00 +0900

    update

--- new-page/build-config.mjs
+++ new-page/build-config.mjs
@@ -3,6 +3,8 @@ export default {
     "node-sass ./current/src/css/style.scss > ./current/src/css/style.css",
     "cd ~/pj/sandbox/new-page/tools && node ./twitter.mjs",
     "cd ~/pj/sandbox/new-page/tools && node ./make-embeded.mjs",
+    "rm ~/pj/sandbox/new-page/data/rendered/*",
+    "rm ~/www/html/contents/twitter/index*",
     "cd ~/pj/sandbox/new-page/tools && node ./make-page.mjs",
     "rollup -c ./rollup.config.js"
   ],
